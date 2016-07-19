d296f4499a0b490b440c8e9550a5304c8a5832a1
Juan Mansor <juan@funne.ly>
2016-07-19 17:50:23 -0300

    Quote symbol before sending it to ri command.

--- lib/docbot/ruby_doc.rb
+++ lib/docbot/ruby_doc.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module Docbot
   class RubyDoc
     def fetch_symbol_doc(symbol)
-      symbol_doc = `bundle exec ri --format=markdown #{symbol} 2>&1`
+      symbol_doc = `bundle exec ri --format=markdown '#{symbol}' 2>&1`
 
       OpenStruct.new(text: symbol_doc, success: $?.success?, symbol: symbol)
     end

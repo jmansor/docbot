aadadbb4804c7d71025610e41f8c112d5a6d964e
Juan Mansor <juan@funne.ly>
2016-07-19 12:55:00 -0300

    Changed ri command output format to 'markdown'.

--- lib/docbot/ruby_doc.rb
+++ lib/docbot/ruby_doc.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module Docbot
   class RubyDoc
     def fetch_symbol_doc(symbol)
-      symbol_doc = `bundle exec ri #{symbol} 2>&1`
+      symbol_doc = `bundle exec ri --format=markdown #{symbol} 2>&1`
 
       OpenStruct.new(symbol_doc: symbol_doc, success: $?.success?)
     end
